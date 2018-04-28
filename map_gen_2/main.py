ce519e7c2889856d583edf1b078b7a10ff5844b0
Xorgon <norman.reggox@gmail.com>
2018-04-28 21:30:49 +0100

    Updated values.

--- map_gen_2/main.py
+++ map_gen_2/main.py
@@ -1,10 +1,10 @@
 from map_gen_2.canvas import MapCanvas
 from map_gen_2.generator import Generator
 
-height = 720
-width = 1280
+height = 1090
+width = 1920
 
-gen = Generator(height, width, 10000, seed="212363264asdff", regularity=5)
+gen = Generator(height, width, 7500, seed="1asnbn8569", regularity=3)
 canvas = MapCanvas(height, width)
-canvas.draw_map(gen)
+canvas.draw_map(gen, False)
 canvas.show_map()
